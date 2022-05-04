2fce4e02acd241042a032ef03d723b00ba27e1dc
xrone <nunoh_11@hotmail.com>
2022-05-04 17:34:23 +0100

    Tests-Part1-Done

--- src/main/Service/ReturnNthString.java
+++ src/main/Service/ReturnNthString.java
@@ -1,2 +1,6 @@
 public class ReturnNthString {
+
+    public String returnString(String nth, int n){
+        return "test";
+    }
 }
--- src/test/java/TestReturnNthString.java
+++ src/test/java/TestReturnNthString.java
@@ -1,2 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class TestReturnNthString {
+
+    ReturnNthString returnNthString;
+
+    @Before
+    public void TestReturnNthString(){
+        returnNthString = new ReturnNthString();
+    }
+
+    @Test
+    public void testReturnStringGivenN1(){
+        assertEquals(returnNthString.returnString("ITCLiNiCal",1), "ITCLNA");
+    }
+
+    @Test
+    public void testReturnStringGivenN2(){
+        assertEquals(returnNthString.returnString("ITCLiNiCal",2), "TLN");
+    }
+
+    @Test
+    public void testReturnStringGivenN3(){
+        assertEquals(returnNthString.returnString("ITCLiNiCal",3), "CNA");
+    }
+
+    @Test
+    public void testReturnStringGivenNBiggerThanWordLength(){
+        assertEquals(returnNthString.returnString("ITCLiNiCal",100), " ");
+    }
+
+    @Test
+    public void testReturnStringGivenNIsNegative(){
+        assertEquals(returnNthString.returnString("ITCLiNiCal",-1), " ");
+    }
+
+
 }
